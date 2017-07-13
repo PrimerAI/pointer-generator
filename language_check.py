ea788a2700dc4d18eaa7e966076788c6b7a01352
Michael Wu <michael@primer.ai>
2017-07-13 11:57:29 -0700

    add file

---
+++ language_check.py
@@ -0,0 +1,113 @@
+
+_prepositions = {
+    'aboard',
+    'about',
+    'above',
+    'across',
+    'after',
+    'against',
+    'along',
+    'amid',
+    'among',
+    'anti',
+    'around',
+    'as',
+    'at',
+    'before',
+    'behind',
+    'below',
+    'beneath',
+    'beside',
+    'besides',
+    'between',
+    'beyond',
+    'but',
+    'by',
+    'concerning',
+    'considering',
+    'despite',
+    'down',
+    'during',
+    'except',
+    'excepting',
+    'excluding',
+    'following',
+    'for',
+    'from',
+    'in',
+    'inside',
+    'into',
+    'like',
+    'minus',
+    'near',
+    'of',
+    'off',
+    'on',
+    'onto',
+    'opposite',
+    'outside',
+    'over',
+    'past',
+    'per',
+    'plus',
+    'regarding',
+    'round',
+    'save',
+    'since',
+    'than',
+    'through',
+    'to',
+    'toward',
+    'towards',
+    'under',
+    'underneath',
+    'unlike',
+    'until',
+    'up',
+    'upon',
+    'versus',
+    'via',
+    'with',
+    'within',
+    'without',
+}
+
+
+def has_repeated_n_gram(tokens, disallowed_n=3):
+    seen_n_grams = set()
+
+    for i in range(len(tokens) - disallowed_n + 1):
+        n_gram = tuple(tokens[i: i + disallowed_n])
+        if n_gram in seen_n_grams:
+            return True
+        seen_n_grams.add(n_gram)
+
+    return False
+
+
+def sent_end_on_preposition(token_strings):
+    for i in range(len(token_strings) - 1):
+        if token_strings[i] in _prepositions and token_strings[i + 1] == '.':
+            return True
+    return False
+
+
+def has_poor_grammar(token_strings):
+    has_open_left_parens = False
+    quote_count = 0
+
+    for token in token_strings:
+        if token == '(':
+            if has_open_left_parens:
+                return True
+            else:
+                has_open_left_parens = True
+        elif token == ')':
+            if has_open_left_parens:
+                has_open_left_parens = False
+            else:
+                return True
+        elif token == '"':
+            quote_count += 1
+
+    return quote_count % 2 == 1
