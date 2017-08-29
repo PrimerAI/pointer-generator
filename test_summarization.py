1b7200d07d4bbf79e7752fb2c1a8228e1fab2425
michael <michael@primer.ai>
2017-08-29 12:17:25 -0700

    sample results, update test

--- test_summarization.py
+++ test_summarization.py
@@ -2,6 +2,7 @@ import json
 from pytest import raises
 
 from decoder import generate_summary
+from primer_core.analytic_pipelines.base.document_pipeline import SingleDocument
 from primer_core.nlp.get_spacy import get_spacy
 
 
@@ -21,25 +22,19 @@ def test_short_input():
 
 def test_real_article():
     """
-    Test that the summary of a real article is sensible. Note: this test is slow (20 seconds not
-    including imports / loading model).
+    Test that the summary of a real article is the same as generated during a local test.
+    Note: this test is slow (20 seconds not including imports / loading model).
     """
     # load data
     with open('test_article.json') as f:
         data = json.load(f)
 
     # compute summary
-    spacy_article = get_spacy()(data['text'])
-    summary, score, llh = generate_summary(spacy_article)
+    doc = SingleDocument(document_id=0, raw={'body': data['article']})
+    summary, score = generate_summary(doc.spacy_text())
 
-    # check summary
+    # check result
     assert isinstance(summary, unicode)
-    assert 250 < len(summary) < 500
-    assert any(c.isupper() for c in summary)
-    assert '.' in summary
-    lower_case_summary = summary.lower()
-    for word in data['required_words']:
-        assert word.lower() in lower_case_summary
+    assert summary == data['expected_summary']
+    assert abs(score - data['expected_score']) < .001
 
-    # check score
-    assert -5. < score < -.1
